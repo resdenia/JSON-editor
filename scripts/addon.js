57693af67012e6f5742e42b372b8d630ce4a53f2
Anton Kolomiiets <resdenia@gmail.com>
2021-03-10 20:35:17 +0200

    added new files

---
+++ scripts/addon.js
@@ -0,0 +1,17 @@
+const jsonSwitcher = document.querySelector('.switcher-json');
+const builderContainer = document.querySelector('.builder-format');
+const jsonContainer = document.querySelector('.json-format');
+
+// tabs
+const tabItems = document.querySelectorAll('.tab-nav-item');
+
+jsonSwitcher.addEventListener('click', (e) => {
+  jsonSwitcher.classList.toggle('active');
+  if (jsonSwitcher.classList.contains('active')) {
+    jsonContainer.style.display = 'block';
+    builderContainer.style.display = 'none';
+  } else {
+    jsonContainer.style.display = 'none';
+    builderContainer.style.display = 'block';
+  }
+});
