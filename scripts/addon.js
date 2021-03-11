24245f3fb511364317928b8f609bfc0ff2823c89
Anton Kolomiiets <resdenia@gmail.com>
2021-03-11 02:16:12 +0200

    add right side UI upload sample log

--- scripts/addon.js
+++ scripts/addon.js
@@ -1,12 +1,29 @@
 const jsonSwitcher = document.querySelector('.switcher-json');
 const builderContainer = document.querySelector('.builder-format');
 const jsonContainer = document.querySelector('.json-format');
-
+const uploadViewContainer = document.querySelector('.uploadView');
 // tabs
 const tabItems = document.querySelectorAll('.tab-nav-item');
 
 tabItems.forEach((tabItem) => {
-  tabItem.addEventListener('click', (e) => {});
+  tabItem.addEventListener('click', (e) => {
+    const itemNoActive = document.querySelector('.tab-nav-item:not(.active)');
+    const itemBodyNoActive = document.querySelector(
+      '.tab-nav-body__item:not(.active)'
+    );
+
+    if (e.target.classList.contains('active')) {
+      return true;
+    }
+
+    document.querySelector('.tab-nav-item.active').classList.remove('active');
+    document
+      .querySelector('.tab-nav-body__item.active')
+      .classList.remove('active');
+
+    itemNoActive.classList.add('active');
+    itemBodyNoActive.classList.add('active');
+  });
 });
 
 jsonSwitcher.addEventListener('click', (e) => {
@@ -19,3 +36,23 @@ jsonSwitcher.addEventListener('click', (e) => {
     builderContainer.style.display = 'block';
   }
 });
+
+const handleUpload = async (e) => {
+  if (e.target.files[0]) {
+    if (e.target.files[0].size > 5 * 1000000) {
+      return alert('File too large. Upload must be less than 5mb.');
+    }
+    const reader = new FileReader();
+    const content = await new Promise((resolve, reject) => {
+      reader.onload = (event) => resolve(event.target.result);
+      reader.onerror = (error) => reject(error);
+      reader.readAsText(e.target.files[0]);
+    });
+    document.querySelector('#samplesBox').value = content;
+    document.querySelector('#acceptSamplesButton').removeAttribute('disabled');
+  }
+};
+
+document
+  .querySelector('#fileUploadButton')
+  .addEventListener('change', handleUpload);
